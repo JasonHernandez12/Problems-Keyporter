e8dd1eabb5db70664f1c9534a1b9ffe693cf8908
David Munguía <eliezerreyes3042007@gmail.com>
2026-03-31 00:20:41 -0600

    Mi primer commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from flask import Flask
 from database import problems
 
@@ -9,3 +10,16 @@ def test():
 
 if __name__ == '__main__':
     app.run(debug=True, port=6000)
+=======
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # Ahora Flask busca el archivo dentro de la carpeta 'templates'
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+>>>>>>> 8efaf9b (Mi primer commit)
