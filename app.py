0ff840e8eadf7005851bfde8e3482a3d80262d59
Fer Guerrero <josueguerrero130@gmail.com>
2026-04-16 00:16:46 -0600

    Subiendo mis avances de problemas2

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def get_db_connection():
     # Usualmente es localhost\SQLEXPRESS o el nombre de tu PC
     conn_str = (
         'DRIVER={ODBC Driver 17 for SQL Server};'
-        'SERVER=ADRIAN_SO\SQLEXPRESS;'
+        'SERVER=7GUERRERO\SQLEXPRESS;'
         'DATABASE=KeyInstituteDB;'
         'Trusted_Connection=yes;'
     )
